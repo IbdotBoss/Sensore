0b8b6780c9456bd2c7e5f168c2931c44266fdacb
preSTAGE <uthmanibrahimkhalil@gmail.com>
2025-11-09 17:48:25 +0000

    added more models

---
+++ Sensore/Models/PatientProfile.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sensore.Models
+{
+    public class PatientProfile
+    {
+        [Key]
+        public int ProfileId { get; set; }
+
+        [Required]
+        public string PatientUserId { get; set; } // Foreign Key
+
+        [ForeignKey("PatientUserId")]
+        public virtual ApplicationUser PatientUser { get; set; }
+
+        public int HighPressureThreshold { get; set; }
+        public int MinAlertArea { get; set; }
+        public int ContactThreshold { get; set; }
+    }
+}
---
+++ Sensore/Models/PressureFrame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sensore.Models
+{
+    public class PressureFrame
+    {
+        [Key]
+        public long FrameId { get; set; }
+
+        [Required]
+        public string PatientUserId { get; set; } // Foreign Key
+
+        [ForeignKey("PatientUserId")]
+        public virtual ApplicationUser PatientUser { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")] // For JSON string
+        public string PressureData { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")] // For JSON string
+        public string ZonalContactArea { get; set; }
+
+        public int PeakPressureIndex { get; set; }
+        public double ContactAreaPercent { get; set; }
+        public bool IsAlertFlagged { get; set; }
+    }
+}
